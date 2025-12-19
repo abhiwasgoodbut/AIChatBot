64400253bdc52c690a35d275691ec56364780b24
abhiwasgoodbut <sabhinay427@gmail.com>
2025-12-20 00:32:31 +0530

    fix

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -12,40 +12,46 @@ import {Toaster} from 'react-hot-toast'
 const Layout = () => {
 
    const {user,loadingUser} = useAppContext()
-  //  user= false
    const [isMenuOpen, setIsMenuOpen] = useState(false)
    const {pathname}= useLocation()
 
    if(pathname === '/loading' || loadingUser) return <Loading/>
 
   return (
-   
     <>
-    <Toaster />
-    {user ? (
-      <div className='dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white'>
-      <div className='flex h-screen w-screen'>
-        <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen= {setIsMenuOpen}/>
-          {/* ✅ CLICK OUTSIDE TO CLOSE */}
+      <Toaster />
+
+      {!isMenuOpen && (
+        <img 
+          onClick={() => setIsMenuOpen(true)} 
+          src={assets.menu_icon} 
+          className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert'
+        />
+      )}
+
+      {user ? (
+        <div className='dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white'>
+          <div className='flex h-screen w-screen'>
+            
+            <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+
+            {/* ✅ CLICK OUTSIDE TO CLOSE */}
             {isMenuOpen && (
               <div 
                 className="fixed inset-0 bg-black/0 md:hidden"
                 onClick={() => setIsMenuOpen(false)}
               />
             )}
-        <Outlet/>
-    </div>
-    </div>
-    ) : (
-      <div className='bg-linear-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen'>
-        <Login/>
-      </div>
-    )}
-    
-    
-     
+
+            <Outlet/>
+          </div>
+        </div>
+      ) : (
+        <div className='bg-linear-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen'>
+          <Login/>
+        </div>
+      )}
     </>
-   
   )
 }
 
