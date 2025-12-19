889015fa29b035a010ccba82bd81a092a5778505
abhiwasgoodbut <sabhinay427@gmail.com>
2025-12-20 00:25:11 +0530

    fiexe

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 
@@ -7,53 +7,39 @@ import './assets/prism.css'
 import Loading from './pages/Loading.jsx'
 import { useAppContext } from './context/AppContext'
 import Login from './pages/Login.jsx'
-import { Toaster } from 'react-hot-toast'
+import {Toaster} from 'react-hot-toast'
 
 const Layout = () => {
-  const { user, loadingUser } = useAppContext()
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const { pathname } = useLocation()
 
-  if (pathname === '/loading' || loadingUser) return <Loading />
+   const {user,loadingUser} = useAppContext()
+  //  user= false
+   const [isMenuOpen, setIsMenuOpen] = useState(false)
+   const {pathname}= useLocation()
+
+   if(pathname === '/loading' || loadingUser) return <Loading/>
 
   return (
+   
     <>
-      <Toaster />
-
-      {/* Mobile Menu Button */}
-      {!isMenuOpen && user && (
-        <img
-          onClick={() => setIsMenuOpen(true)}
-          src={assets.menu_icon}
-          className="fixed top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert z-50"
-        />
-      )}
-
-      {user ? (
-        <div className='dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white h-[100dvh] w-screen flex relative overflow-hidden'>
-
-          {/* Sidebar */}
-          <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-
-          {/* Overlay for mobile */}
-          {isMenuOpen && (
-            <div
-              className="fixed inset-0 bg-black/50 md:hidden z-30"
-              onClick={() => setIsMenuOpen(false)}
-            />
-          )}
-
-          {/* Main Chat Area */}
-          <div className="flex-1 h-dvh overflow-hidden">
-            <Outlet />
-          </div>
-        </div>
-      ) : (
-        <div className='bg-linear-to-b from-[#242124] to-[#000000] flex items-center justify-center h-[100dvh] w-screen'>
-          <Login />
-        </div>
-      )}
+    <Toaster />
+    {!isMenuOpen && <img onClick={() => setIsMenuOpen(true)} src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert'/>}
+    {user ? (
+      <div className='dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white'>
+      <div className='flex h-screen w-screen'>
+        <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen= {setIsMenuOpen}/>
+        <Outlet/>
+    </div>
+    </div>
+    ) : (
+      <div className='bg-linear-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen'>
+        <Login/>
+      </div>
+    )}
+    
+    
+     
     </>
+   
   )
 }
 
